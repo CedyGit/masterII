f51b383b93ef62cc398ee5afbdb83a2f93247e28
cedyGit <cedricmiandra33@gmail.com>
2025-11-30 15:54:15 +0100

    ok projet

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/map', function () {
     return view('map');
 });
